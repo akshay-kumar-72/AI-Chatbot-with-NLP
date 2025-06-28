4c37176b5907b2ab12bbf8dfc3567994e725ec63
akshay-kumar-72 <akshaykumar725296@gmail.com>
2025-06-27 23:44:26 -0500

    Update settings.py

--- Chatbot-NLP/settings.py
+++ Chatbot-NLP/settings.py
@@ -1,1 +1,19 @@
+INSTALLED_APPS = [
+    ...
+    'chat',
+    'channels',
+    'rest_framework',
+]
+
+ASGI_APPLICATION = 'chatbot_nlp.asgi.application'
+
+# Redis for Channels
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
