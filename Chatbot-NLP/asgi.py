bc280dd3d2cac1b099488b069ac27799b0f79c49
akshay-kumar-72 <akshaykumar725296@gmail.com>
2025-06-27 23:44:53 -0500

    Update asgi.py

--- Chatbot-NLP/asgi.py
+++ Chatbot-NLP/asgi.py
@@ -1,1 +1,8 @@
+import os
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbot_nlp.settings')
+django.setup()
+application = get_default_application()
 
