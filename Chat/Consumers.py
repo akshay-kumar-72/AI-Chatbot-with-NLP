76b4a7247a42cef97f72c6e3ff056fe4ceb8201f
akshay-kumar-72 <akshaykumar725296@gmail.com>
2025-06-27 23:47:43 -0500

    Update Consumers.py

--- Chat/Consumers.py
+++ Chat/Consumers.py
@@ -1,1 +1,18 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from .nlp import get_bot_response
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        response = get_bot_response(message)
+
+        await self.send(text_data=json.dumps({
+            'message': response
+        }))
 
